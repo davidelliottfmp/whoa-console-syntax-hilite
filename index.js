da541b71b337009473792cec8e2c1312ed8f0cc8
David Elliott <dave.elliott@codevelop.co.uk>
2017-06-25 14:56:14 +0100

    Still Spiking

--- index.js
+++ index.js
@@ -13,47 +13,44 @@ console.log(unescapedHtmlString);
 var obj = css.parse('body { font-size: 12px; }', {});
 console.log(css.stringify(obj, {}));
 
-const path = getInstalledPath.sync('highlight.js', {
-  local: true,
-});
-console.log(path);
+console.log('\x1b[38;2;255;100;0mTRUECOLOR\x1b[0m\n');
 
-// <span> </span>
+const cssObject = loadStyle('dracula');
+const conversionTable = generateHTMLToConsoleConversionTable(cssObject);
+console.log(cssObject.stylesheet);
 
-//
-//
-// Reset = "\x1b[0m"
-// Bright = "\x1b[1m"
-// Dim = "\x1b[2m"
-// Underscore = "\x1b[4m"
-// Blink = "\x1b[5m"
-// Reverse = "\x1b[7m"
-// Hidden = "\x1b[8m"
-//
-// FgBlack = "\x1b[30m"
-// FgRed = "\x1b[31m"
-// FgGreen = "\x1b[32m"
-// FgYellow = "\x1b[33m"
-// FgBlue = "\x1b[34m"
-// FgMagenta = "\x1b[35m"
-// FgCyan = "\x1b[36m"
-// FgWhite = "\x1b[37m"
-//
-// BgBlack = "\x1b[40m"
-// BgRed = "\x1b[41m"
-// BgGreen = "\x1b[42m"
-// BgYellow = "\x1b[43m"
-// BgBlue = "\x1b[44m"
-// BgMagenta = "\x1b[45m"
-// BgCyan = "\x1b[46m"
-// BgWhite = "\x1b[47m"
+function generateHTMLToConsoleConversionTable(cssObject) {
+  cssObject.stylesheet.rules.forEach(rule => {
+    if (rule.type == 'rule') {
+      rule.declarations.forEach(declaration => {
+        if (declaration == 'color') {
+          rule.selectors.forEach(cssName => {
+            console.log(`<span class="${cssName}"> becomes `);
+          });
+          console.log('----------------------------');
+          console.log(rule.declarations);
+        }
+      });
+    }
+  });
+}
 
-// black
-// red
-// green
-// yellow
-// blue (on Windows the bright version is used as normal blue is illegible)
-// magenta
-// cyan
-// white
-// gray
+function replaceAllEndSpansWithReset(code) {
+  return code.replace('</span>', '\x1b[0m');
+}
+
+function loadStyle(style) {
+  const pathToStyle = _getPathToStyleFolder();
+  const cssFileContents = fs.readFileSync(
+    `${pathToStyle}/${style}.css`,
+    'utf8'
+  );
+  return cssFileContents && css.parse(cssFileContents, {});
+}
+
+function _getPathToStyleFolder() {
+  const path = getInstalledPath.sync('highlight.js', {
+    local: true
+  });
+  return path + '/styles';
+}
