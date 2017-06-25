387d5ea4ec6d406de1be1db99fa1e3ab0e106f0d
David Elliott <dave.elliott@codevelop.co.uk>
2017-06-25 20:32:11 +0100

    Found styles

--- index.js
+++ index.js
@@ -6,33 +6,39 @@ const getInstalledPath = require('get-installed-path');
 
 const sourceFile = fs.readFileSync('index.js', 'utf8');
 const htmlString = hljs.highlight('js', sourceFile).value;
-
+console.log(htmlString);
 const unescapedHtmlString = he.unescape(htmlString);
 
-console.log(unescapedHtmlString);
+// console.log(unescapedHtmlString);
 var obj = css.parse('body { font-size: 12px; }', {});
-console.log(css.stringify(obj, {}));
+// console.log(css.stringify(obj, {}));
 
 console.log('\x1b[38;2;255;100;0mTRUECOLOR\x1b[0m\n');
 
-const cssObject = loadStyle('dracula');
+const cssObject = loadStyle('agate');
 const conversionTable = generateHTMLToConsoleConversionTable(cssObject);
 console.log(cssObject.stylesheet);
 
 function generateHTMLToConsoleConversionTable(cssObject) {
+  var lookupObject = {};
+  console.log(JSON.stringify(cssObject));
   cssObject.stylesheet.rules.forEach(rule => {
     if (rule.type == 'rule') {
       rule.declarations.forEach(declaration => {
-        if (declaration == 'color') {
+        if (declaration.property == 'color') {
           rule.selectors.forEach(cssName => {
-            console.log(`<span class="${cssName}"> becomes `);
+            const splitNames = cssName.split(' ');
+            splitNames.forEach(splitCssName => {
+              lookupObject[
+                `<span class="${splitCssName.substr(1)}">`
+              ] = `${declaration.value}`;
+            });
           });
-          console.log('----------------------------');
-          console.log(rule.declarations);
         }
       });
     }
   });
+  return lookupObject;
 }
 
 function replaceAllEndSpansWithReset(code) {
